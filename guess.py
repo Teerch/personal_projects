246f40e8724072cf9c517ad3f95ffc313a399e6b
Teerch <98260847+Teerch@users.noreply.github.com>
2023-07-30 15:29:17 +0100

    Updated guess.py by creating a function guessing_game

--- guess.py
+++ guess.py
@@ -2,26 +2,58 @@ from random import randint
 import time
 
 
-rand_num = randint(1, 10)
-guess = None
-
-while True:
-    try:       
-        guess = input("pick a number between 1 and 10\n")
-        guess = int(guess)
-        if guess < rand_num:
-            print("too low")
-        elif guess > rand_num:
-            print("too high")
-        else:
-            print("you won")
-            play_again = input("Do you want to play again? (y/n) \n")
-            if play_again == "y":
-                rand_num = randint(1, 10)
-                guess = None
+def guessing_game():
+    rand_num = randint(1, 10)
+    tries = 3
+    while True:
+        try:
+        
+            guess = input("guess a number between 1 and 10:\n")
+            guess = int(guess)
+    
+            if guess == rand_num:
+                print("you win")
+                play_again = input("Do you want to play again? (y/n) ")
+                if play_again == "y":
+                    rand_num = randint(1, 10)
+                    guess = ""
+                    guess = int(guess)
+                else:
+                    print("thanks for playing")
+                    sleep(1.5)
+                    break
+                
+            if guess < rand_num or guess > rand_num or guess == rand_num:
+                tries = tries - 1
+                if tries == 2:
+                    print("2 tries left")
+                elif tries == 1:
+                    print("1 try left")
+                else:
+                    print("you lose")
+                    sleep(1.5)
+                    break
+            if guess < rand_num:
+                print("too low, try again")
+            elif guess > rand_num:
+                print("too high, try again")
             else:
-                print("Thanks for playing!")
-                time.sleep(1.5)
-                break
-    except (ValueError):
-        print("Select a number")
+                print("good job, you guessed correctly")
+                play_again = input("Do you want to play again? (y/n) ")
+                if play_again == "y":
+                    rand_num = randint(1, 10)
+                    guess = None
+                else:
+                    print("thanks for playing")
+                    sleep(1.5)
+                    break
+                
+        except (ValueError):
+            print("Select a number")
+
+name = input("Welcome enter your name\n")
+ans = input(f"Would you like to play a game {name}? y/n:")
+if ans == "y":
+    guessing_game()
+else:
+    print("Goodbye")
